8df5afdc36f3620ab391db79cda15667799934ed
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-12-03 17:20:26 -0600

    Create blockColors.py

---
+++ blockColors.py
@@ -0,0 +1,17 @@
+blocks = {
+    "grass": "#4e762a",
+    "dirt": "#866043",
+    "stone": "#7d7d7d",
+    "sand": "#dbcfa3",
+    "gravel": "#837f7e",
+    "clay": "#a0a6b3",
+    "leaves": "#194306",
+    "leaves2": "#194306",
+    "log": "#6d5532",
+    "planks": "#a2824e",
+    "water": "#7287fd",
+    "tallgrass": "#4e762a",
+    "double_plant": "#4e762a",
+    "redstone_block": "#ab1c09",
+    "lapis_block": "#27438a"
+}
